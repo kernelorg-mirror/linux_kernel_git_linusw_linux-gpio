27a75511430d97b8114640d16e95c2c1f0c16e96
Linus Walleij <linus.walleij@linaro.org>
2024-06-30 23:30:20 +0200

    ASoC: tas2781-i2c: Drop weird GPIO code
    The tas2781-i2c driver gets an IRQ from either ACPI or device tree,
    then proceeds to check if the IRQ has a corresponding GPIO and in
    case it does enforce the GPIO as input and set a label on it.
    
    This is abuse of the API:
    
    - First we cannot guarantee that the numberspaces of the GPIOs and
      the IRQs are the same, i.e that an IRQ number corresponds to
      a GPIO number like that.
    
    - Second, GPIO chips and IRQ chips should be treated as orthogonal
      APIs, the irqchip needs to ascertain that the backing GPIO line
      is set to input etc just using the irqchip.
    
    - Third it is using the legacy <linux/gpio.h> API which should not
      be used in new code yet this was added just a year ago.
    
    Delete the offending code.
    
    If this creates problems the GPIO and irqchip maintainers can help
    to fix the issues.
    
    It *should* not create any problems, because the irq isn't
    used anywhere in the driver, it's just obtained and then
    left unused.
    
    Fixes: ef3bcde75d06 ("ASoC: tas2781: Add tas2781 driver")
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- sound/soc/codecs/tas2781-i2c.c
+++ sound/soc/codecs/tas2781-i2c.c
@@ -21,7 +21,6 @@
 #include <linux/interrupt.h>
 #include <linux/module.h>
 #include <linux/of.h>
-#include <linux/of_gpio.h>
 #include <linux/of_irq.h>
 #include <linux/regmap.h>
 #include <linux/slab.h>
@@ -605,7 +604,7 @@ static void tasdevice_parse_dt(struct tasdevice_priv *tas_priv)
 {
 	struct i2c_client *client = (struct i2c_client *)tas_priv->client;
 	unsigned int dev_addrs[TASDEVICE_MAX_CHANNELS];
-	int rc, i, ndev = 0;
+	int i, ndev = 0;
 
 	if (tas_priv->isacpi) {
 		ndev = device_property_read_u32_array(&client->dev,
@@ -620,7 +619,7 @@ static void tasdevice_parse_dt(struct tasdevice_priv *tas_priv)
 				"ti,audio-slots", dev_addrs, ndev);
 		}
 
-		tas_priv->irq_info.irq_gpio =
+		tas_priv->irq =
 			acpi_dev_gpio_irq_get(ACPI_COMPANION(&client->dev), 0);
 	} else {
 		struct device_node *np = tas_priv->dev->of_node;
@@ -648,7 +647,7 @@ static void tasdevice_parse_dt(struct tasdevice_priv *tas_priv)
 		ndev = 1;
 		dev_addrs[0] = client->addr;
 #endif
-		tas_priv->irq_info.irq_gpio = of_irq_get(np, 0);
+		tas_priv->irq = of_irq_get(np, 0);
 	}
 	tas_priv->ndev = ndev;
 	for (i = 0; i < ndev; i++)
@@ -661,23 +660,6 @@ static void tasdevice_parse_dt(struct tasdevice_priv *tas_priv)
 			__func__);
 
 	strcpy(tas_priv->dev_name, tasdevice_id[tas_priv->chip_id].name);
-
-	if (gpio_is_valid(tas_priv->irq_info.irq_gpio)) {
-		rc = gpio_request(tas_priv->irq_info.irq_gpio,
-				"AUDEV-IRQ");
-		if (!rc) {
-			gpio_direction_input(
-				tas_priv->irq_info.irq_gpio);
-
-			tas_priv->irq_info.irq =
-				gpio_to_irq(tas_priv->irq_info.irq_gpio);
-		} else
-			dev_err(tas_priv->dev, "%s: GPIO %d request error\n",
-				__func__, tas_priv->irq_info.irq_gpio);
-	} else
-		dev_err(tas_priv->dev,
-			"Looking up irq-gpio property failed %d\n",
-			tas_priv->irq_info.irq_gpio);
 }
 
 static int tasdevice_i2c_probe(struct i2c_client *i2c)
